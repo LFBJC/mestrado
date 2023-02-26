bd2f16ce257981920be75197b63c6d65062cc5f8
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2023-02-26 12:17:38 -0300

    plot do valor verdadeiro vs valor predito
    No momento estou usando um plot de múltiplas linhas em vez de um boxplot

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tensorflow.keras import models, layers, losses
 
 # Load the data from Yahoo Finance
 data = yf.download('AAPL')
+data.drop("Volume", axis="columns", inplace=True)
 print(data)
 data.reset_index(inplace=True)
 
@@ -16,6 +17,7 @@ def normalization(x, batch_index):
     delta = (batch_data.max()-batch_data.min())
     x_minus_min = (x - batch_data.min())
     return x_minus_min/delta
+
 
 def inverse_normalization(x, batch_index):
     global data
@@ -41,19 +43,20 @@ for batch_index in range(data.shape[0]//10-1):
 
 images = np.array(images)
 labels = np.array(labels)
-train_images, val_images = images[:int(0.8*images.shape[0])], images[int(0.8*images.shape[0]):]
-train_labels, val_labels = labels[:int(0.8*labels.shape[0])], labels[int(0.8*labels.shape[0]):]
+train_images, val_images, test_images = images[:int(0.5*images.shape[0])], images[int(0.5*images.shape[0]):int(0.75*images.shape[0])], images[int(0.75*images.shape[0]):]
+train_labels, val_labels, test_labels = labels[:int(0.5*labels.shape[0])], labels[int(0.5*labels.shape[0]):int(0.75*images.shape[0])], labels[int(0.75*images.shape[0]):]
 train_images = tf.convert_to_tensor(train_images[:, :, :, np.newaxis], dtype=tf.float32)
 train_labels = tf.convert_to_tensor(train_labels, dtype=tf.float32)
 val_images = tf.convert_to_tensor(val_images[:, :, :, np.newaxis], dtype=tf.float32)
 val_labels = tf.convert_to_tensor(val_labels, dtype=tf.float32)
+test_images = tf.convert_to_tensor(test_images[:, :, :, np.newaxis], dtype=tf.float32)
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(9, 7, 1)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(9, len(data.columns), 1)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 2), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(7))
+model.add(layers.Dense(len(data.columns)))
 model.summary()
 model.compile(optimizer='adam',
               loss=losses.MeanSquaredError(),
@@ -68,4 +71,15 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+# Predicted vs actual
+test_labels = [inverse_normalization(x, batch_index) for batch_index, x in enumerate(test_labels)]
+predicted_labels = [inverse_normalization(pred, batch_index) for batch_index, pred in enumerate(model.predict(test_images))]
+plot_labels = []
+for i, c in enumerate(data.columns):
+    plt.plot([batch_data[c] for batch_data in test_labels])
+    plt.plot([pred_data[i] for pred_data in predicted_labels])
+    plot_labels.extend([f'actual {c}', f'predicted {c}'])
+plt.title('actual vs predicted')
+plt.legend(plot_labels, loc='upper left')
 plt.show()
