5353b6aaa9ff14f06856e5e4e9576b5cdcccddf5
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2023-08-03 09:20:45 -0300

    Otimização de hiperparâmetros de alguns datasets

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,4 +1,6 @@
 import os
+
+import matplotlib.pyplot as plt
 import optuna
 import numpy as np
 import pandas as pd
@@ -9,7 +11,7 @@ from utils import create_model, MMRE, images_and_targets_from_data_series
 def objective(trial, study, data_set_index):
     train_data = pd.read_csv(f'data/{data_set_index}/train.csv').to_dict('records')
     val_data = pd.read_csv(f'data/{data_set_index}/val.csv').to_dict('records')
-    os.makedirs(f'best_models/{data_set_index}', exist_ok=True)
+    os.makedirs(f'hyperparameter_optimization_output/{data_set_index}', exist_ok=True)
     win_size = trial.suggest_int('win_size', 10, 40)
     filters_conv_1 = trial.suggest_int('filters_conv_1', 10, 30)
     kernel_size_conv_1 = (
@@ -57,10 +59,12 @@ def objective(trial, study, data_set_index):
     )
     N_EPOCHS = 50
     epochs_no_improve = 0
+    train_losses = []
     val_losses = []
     X = np.zeros((1, win_size, 5, 1))
     for epoch in range(N_EPOCHS):
         if epochs_no_improve < 10:
+            train_losses_for_this_epoch = []
             for image, target_bbox, inverse_normalization in images_and_targets_from_data_series(
                     train_data, input_win_size=win_size
             ):
@@ -74,6 +78,8 @@ def objective(trial, study, data_set_index):
 
                 # train the model on the input-output pair for one epoch
                 model.fit(X, y, batch_size=1, epochs=1, verbose=0)
+                train_losses_for_this_epoch.append(MMRE(y, model.predict(X, verbose=0)))
+            train_losses.append(np.mean(train_losses_for_this_epoch))
 
             val_losses_for_this_epoch = []
             for image, target_bbox, inverse_normalization in images_and_targets_from_data_series(
@@ -93,17 +99,30 @@ def objective(trial, study, data_set_index):
     error = val_losses[-1]
     try:
         if error < study.best_value:
-            model.save(f'best_models/{data_set_index}/best_model.h5')
+            model.save(f'hyperparameter_optimization_output/{data_set_index}/best_model.h5')
+            plt.figure()
+            plt.plot(train_losses, label='train')
+            plt.plot(val_losses, label='val')
+            plt.legend()
+            plt.savefig(f'hyperparameter_optimization_output/{data_set_index}/best_model_train_hist.png')
     except ValueError:
         # if no trials are completed yet save the first trial
-        model.save(f'best_models/{data_set_index}/best_model.h5')
+        model.save(f'hyperparameter_optimization_output/{data_set_index}/best_model.h5')
+        plt.figure()
+        plt.plot(train_losses, label='train')
+        plt.plot(val_losses, label='val')
+        plt.legend()
+        plt.savefig(f'hyperparameter_optimization_output/{data_set_index}/best_model_train_hist.png')
+    opt_hist_df = pd.DataFrame.from_records([trial.params])
+    opt_hist_df['score'] = error
+    opt_hist_df.to_csv(f'hyperparameter_optimization_output/{data_set_index}/opt_hist.csv', mode='a')
     return error
 
 
-data_set_index = 0
+data_set_index = 3
 study = optuna.create_study(
     direction='minimize',
     pruner=optuna.pruners.MedianPruner(n_startup_trials=30),
-    study_name='hyperparameter_opt'
+    study_name=f'hyperparameter_opt_{data_set_index}'
 )
 study.optimize(lambda trial: objective(trial, study, data_set_index), n_trials=100, n_jobs=2)
