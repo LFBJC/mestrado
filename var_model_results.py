ed55d5ec8758574481d421c122297ebfaf0a31a2
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2023-09-05 19:54:45 -0300

    Dados gerados com a seleção de protótipos + função para fazer o resultado da seleção de protótipos se tornar uma série de boxplots + adaptação do código do VAR para usar os dados novos + adaptação da geração de dados para gerar por partes

--- var_model_results.py
+++ var_model_results.py
@@ -3,7 +3,26 @@ import pandas as pd
 import numpy as np
 from statsmodels.tsa.api import VAR
 
-for filename in os.listdir('data'):
+total_seeds = 1000
+if os.path.exists('results_summary.csv'):
+    results_df = pd.read_csv('results_summary.csv')
+    max_data_index = max(results_df['Data_index'])
+    if max_data_index < total_seeds - 1:
+        results_df = pd.concat([
+            results_df,
+            pd.DataFrame({
+                'Data_index': np.arange(max_data_index, total_seeds),
+                'VAR_result': [np.nan] * (total_seeds - max_data_index),
+                'CNN_result': [np.nan] * (total_seeds - max_data_index)
+            })
+        ], ignore_index=True)
+else:
+    results_df = pd.DataFrame({
+        'Data_index': np.arange(total_seeds), 'VAR_result': [np.nan] * total_seeds, 'CNN_result': [np.nan] * total_seeds,
+    })
+indices_to_use = np.arange(35,50)
+for data_index in indices_to_use:
+    filename = f'{data_index}.csv'
     print('#' * 80)
     print(filename)
     df = pd.read_csv('data/'+filename)
@@ -18,6 +37,8 @@ for filename in os.listdir('data'):
 
     # Calcular o MMRE
     mmre = np.mean(relative_errors)
+    results_df.loc[results_df['Data_index'] == data_index, 'VAR_result'] = mmre
 
     print("Mean Magnitude of Relative Errors (MMRE):", mmre)
     print('#' * 80)
+results_df.to_csv('results_summary.csv', index=False)
