1c67313fa70e95d2e94948b9b26f97609dcf42ef
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2023-11-16 13:05:20 -0300

    Resultados para 60 conjuntos de dados e passando informações importantes para uma pasta separada do repositório para economizar espaço

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -7,12 +7,11 @@ import pandas as pd
 from utils import create_model, MMRE, images_and_targets_from_data_series
 from tqdm import tqdm
 import tensorflow as tf
+caminho_de_saida = "E:/mestrado/Pesquisa/Dados simulados/Saída da otimização de hiperparâmetros"
 
-steps_ahead = [1, 5, 20]
-
-def objective(trial, study, data_set_index):
+def objective(trial, study, data_set_index, steps_ahead):
     train_data = pd.read_csv(f'data/{data_set_index}.csv').to_dict('records')
-    os.makedirs(f'hyperparameter_optimization_output/{data_set_index}', exist_ok=True)
+    os.makedirs(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}', exist_ok=True)
     win_size = trial.suggest_int('win_size', 100, 800)
     filters_conv_1 = trial.suggest_int('filters_conv_1', 12, 50)
     kernel_size_conv_1 = (
@@ -38,9 +37,6 @@ def objective(trial, study, data_set_index):
     activation_conv_2 = trial.suggest_categorical('activation_conv_2',
                                                   ['relu', 'elu', 'sigmoid', 'linear', 'tanh', 'swish'])
     dense_neurons = trial.suggest_int('dense_neurons', 5, w4*h4*filters_conv_2 - 1)
-    dense_activation = trial.suggest_categorical(
-        'dense_activation', ['relu', 'elu', 'sigmoid', 'linear', 'tanh', 'swish']
-    )
     model = create_model(
         input_shape=(win_size, 5, 1),
         filters_conv_1=filters_conv_1, kernel_size_conv_1=kernel_size_conv_1,
@@ -48,7 +44,7 @@ def objective(trial, study, data_set_index):
         pool_size_1=pool_size_1, pool_type_1=pool_type_1,
         filters_conv_2=filters_conv_2, kernel_size_conv_2=kernel_size_conv_2,
         activation_conv_2=activation_conv_2,
-        dense_neurons=dense_neurons, dense_activation=dense_activation
+        dense_neurons=dense_neurons, dense_activation='sigmoid'
     )
     model.compile(
         optimizer=trial.suggest_categorical('optimizer', ['adam', 'adadelta', 'adagrad', 'rmsprop', 'sgd']),
@@ -59,40 +55,93 @@ def objective(trial, study, data_set_index):
         metrics=[MMRE]
     )
     N_EPOCHS = 50
-    train_losses_by_steps_ahead = []
     X, Y, inverse_normalizations = images_and_targets_from_data_series(
-        train_data, input_win_size=win_size, steps_ahead=max(steps_ahead)
+        train_data, input_win_size=win_size, steps_ahead=steps_ahead
     )
     Y_ = Y
     Y_[:, :, 1:] -= Y_[:, :, :-1]
     model.fit(X, Y_[:, 0, :], batch_size=X.shape[0], epochs=N_EPOCHS, verbose=0)
     T = model.predict(X, verbose=0)
-    if 1 in steps_ahead:
-        train_losses_by_steps_ahead.append(tf.keras.backend.eval(MMRE(Y_[:, 0, :], T)))
-    for s in range(2, max(steps_ahead)+1):
-        T = model.predict(X, verbose=0).reshape(T.shape[0], 1, -1, 1)
-        X_ = np.concatenate([X[:, 1:, :, :], T], axis=1)
-        if s in steps_ahead:
-            train_losses_by_steps_ahead.append(
-                tf.keras.backend.eval(MMRE(Y_[:, s-1, :], model.predict(X_)))
-            )
-    error = np.mean(train_losses_by_steps_ahead)
+    error = 0
+    if steps_ahead == 1:
+        error = tf.keras.backend.eval(MMRE(Y_[:, 0, :], T))
+    else:
+        for s in range(2, steps_ahead+1):
+            T = model.predict(X, verbose=0).reshape(T.shape[0], 1, -1, 1)
+            X_ = np.concatenate([X[:, 1:, :, :], T], axis=1)
+            if s == steps_ahead:
+                error = tf.keras.backend.eval(MMRE(Y_[:, s-1, :], model.predict(X_)))
     if np.isnan(error):
-        error = -1
+        print(debug)
+    try:
+        if error < study.best_value:
+            model.save(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}/best_model.h5')
+    except ValueError:
+        # if no trials are completed yet save the first trial
+        model.save(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}/best_model.h5')
     opt_hist_df = pd.DataFrame.from_records([trial.params])
     opt_hist_df['w2'] = w2
     opt_hist_df['w3'] = w3
     opt_hist_df['w4'] = w4
     opt_hist_df['h4'] = h4
     opt_hist_df['score'] = error
-    opt_hist_df.to_csv(f'hyperparameter_optimization_output/opt_hist.csv', mode='a', index=False, header=(trial.number == 0))
+    hist_path = f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}/opt_hist.csv'
+    append_condition = os.path.exists(hist_path)
+    print('append_condition:', append_condition)
+    opt_hist_df.to_csv(hist_path, mode='a' if append_condition else 'w', index=False, header=(not append_condition))
     return error
 
 
-data_set_index = 0
-study = optuna.create_study(
-    direction='minimize',
-    pruner=optuna.pruners.MedianPruner(n_startup_trials=30),
-    study_name=f'hyperparameter_opt_{data_set_index}'
-)
-study.optimize(lambda trial: objective(trial, study, data_set_index), n_trials=200)
+steps_ahead = 1 # [1, 5, 20]
+for data_set_index in range(100):
+    os.makedirs(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}', exist_ok=True)
+    if not os.path.exists(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}/opt_hist.csv'):
+        study = optuna.create_study(
+            direction='minimize',
+            pruner=optuna.pruners.MedianPruner(n_startup_trials=30),
+            study_name=f'hyperparameter_opt_{data_set_index}'
+        )
+        study.optimize(lambda trial: objective(trial, study, data_set_index, steps_ahead), n_trials=100)
+    else:
+        df_temp = pd.read_csv(f'{caminho_de_saida}/{steps_ahead} steps ahead/{data_set_index}/opt_hist.csv')
+        if df_temp.shape[0] == 0:
+            study = optuna.create_study(
+                direction='minimize',
+                pruner=optuna.pruners.MedianPruner(n_startup_trials=30),
+                study_name=f'hyperparameter_opt_{data_set_index}'
+            )
+            study.optimize(lambda trial: objective(trial, study, data_set_index, steps_ahead), n_trials=100)
+        elif df_temp.shape[0] < 100:
+            study = optuna.create_study(
+                direction='minimize',
+                study_name=f'hyperparameter_opt_{data_set_index}'
+            )
+            for _, row in df_temp.iterrows():
+                study.add_trial(
+                    optuna.trial.create_trial(
+                        params={c: v for c, v in row.to_dict().items() if c not in ['w2', 'w3', 'w4', 'h4', 'score']},
+                        distributions={
+                            'win_size': optuna.distributions.IntDistribution(100, 800),
+                            'filters_conv_1': optuna.distributions.IntDistribution(12, 50),
+                            'kernel_size_conv_1[0]': optuna.distributions.IntDistribution(row['win_size']//5, row['win_size']//3),
+                            'kernel_size_conv_1[1]': optuna.distributions.IntDistribution(1, 5),
+                            'activation_conv_1': optuna.distributions.CategoricalDistribution(['relu', 'elu', 'sigmoid', 'linear', 'tanh', 'swish']),
+                            'pool_size_1[0]': optuna.distributions.IntDistribution(1, (row['win_size'] - row['kernel_size_conv_1[0]'] + 1)//2),
+                            'pool_type_1': optuna.distributions.CategoricalDistribution(['max', 'average']),
+                            'filters_conv_2': optuna.distributions.IntDistribution(6, row['filters_conv_1'] // 2),
+                            'kernel_size_conv_2[0]': optuna.distributions.IntDistribution(1, row['w3'] - 1),
+                            'activation_conv_2': optuna.distributions.CategoricalDistribution(['relu', 'elu', 'sigmoid', 'linear', 'tanh', 'swish']),
+                            'dense_neurons': optuna.distributions.IntDistribution(5, row['w4']*row['h4']*row['filters_conv_2'] - 1),
+                            'optimizer': optuna.distributions.CategoricalDistribution(['adam', 'adadelta', 'adagrad', 'rmsprop', 'sgd']),
+                            'loss': optuna.distributions.CategoricalDistribution([
+                                'mean_squared_error', 'mean_squared_logarithmic_error', 'mean_absolute_percentage_error',
+                                'mean_absolute_error'
+                            ])
+                        },
+                        value=row['score']
+                    )
+                )
+            study.optimize(lambda trial: objective(trial, study, data_set_index, steps_ahead), n_trials=100-df_temp.shape[0])
+            del df_temp
+        else:
+            del df_temp
