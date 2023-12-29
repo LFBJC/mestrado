0f5f11d12ab199c143e8f7b6bebe70849e63e21e
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2023-12-29 14:07:00 -0300

    Usando a distribuição normal
    Não sei qual eu usava antes, mas é bom seguir a que Dailys usa e principalmente saber o que estou fazendo

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ def random_walk(n_samples: int = 10000, begin_value: float=None):
         begin_value = np.random.rand()
     out = [begin_value]
     for step in range(n_samples-1):
-        out.append(out[-1] + (np.random.rand()*2 - 1))
+        out.append(out[-1] + (np.random.normal()*2 - 1))
     return out
 
 
