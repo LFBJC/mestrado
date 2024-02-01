68609ca39e8dd31f6c4f6403cdeac8fac7be3dba
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2024-01-31 22:14:39 -0300

    Várias mudanças
    Incluindo LSTM
    Correção de erro no teste do VAR
    Correção de erro na geração da série

--- var_model_results.py
+++ var_model_results.py
@@ -2,47 +2,58 @@ import os
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.api import VAR
+from utils import plot_single_box_plot_series, plot_multiple_box_plot_series
 
 caminho_de_saida = "E:/mestrado/Pesquisa/Dados simulados/resultados VAR.csv"
-total_seeds = 1000
+num_sets = 10
+lags = 1
 steps_ahead_list = [1, 5, 20]
 if os.path.exists(caminho_de_saida):
     results_df = pd.read_csv(caminho_de_saida)
     max_data_index = max(results_df['Data_index'])
-    if max_data_index < total_seeds - 1:
+    if max_data_index < num_sets - 1:
         results_df = pd.concat([
             results_df,
             pd.DataFrame({
-                'Data_index': np.arange(max_data_index, total_seeds)
+                'Data_index': np.arange(max_data_index, num_sets)
             })
         ], ignore_index=True)
 else:
     results_df = pd.DataFrame({
-        'Data_index': np.arange(total_seeds)
+        'Data_index': np.arange(num_sets)
     })
     for steps_ahead in steps_ahead_list:
-        results_df[f'VAR_result ({steps_ahead} steps ahead)'] = [np.nan] * total_seeds
-indices_to_use = np.arange(1000)
+        results_df[f'VAR_result ({steps_ahead} steps ahead)'] = [np.nan] * num_sets
+indices_to_use = np.arange(num_sets)
 for data_index in indices_to_use:
     for steps_ahead in steps_ahead_list:
-        filename = f'{data_index}.csv'
-        print('#' * 80)
-        print(filename)
-        print(f'{steps_ahead} steps ahead')
-        df = pd.read_csv('E:/mestrado/Pesquisa/Dados simulados/Dados/'+filename)
-        model = VAR(df)
-        model_fitted = model.fit(1)
+        if np.isnan(results_df[f'VAR_result ({steps_ahead} steps ahead)'].iloc[data_index]):
+            df = pd.read_csv(f'E:/mestrado/Pesquisa/Dados simulados/Dados/config 1/{data_index}/train.csv')
+            # plot_single_box_plot_series(df.to_dict('records'), title='Dados de Treinamento')
+            model = VAR(df)
+            model_fitted = model.fit(maxlags=lags)
+            df_test = pd.read_csv(f'E:/mestrado/Pesquisa/Dados simulados/Dados/config 1/{data_index}/test.csv')
 
-        # Fazer previsões para o próximo período
-        forecast = model_fitted.forecast(df.values, steps=steps_ahead)
+            relative_errors = []
+            entradas_teste = []
+            series_forecasts = []
+            series_targets = []
+            for i, row in df_test.iterrows():
+                if i + lags + steps_ahead < df_test.shape[0]:
+                    # Fazer previsões para o próximo período
+                    entradas_teste.extend(df_test.iloc[i:i+lags].to_dict('records'))
+                    forecast = model_fitted.forecast(df_test.values[i:i+lags], steps=steps_ahead)[0]
+                    series_forecasts.append({c: v for c, v in zip(df_test.columns, forecast)})
+                    series_targets.append(df_test.iloc[i+steps_ahead+lags].to_dict())
+                    # Calcular os erros relativos
+                    relative_errors.append(np.abs((df_test.values[i+steps_ahead+lags] - forecast) / (forecast + 0.0001)))
+            # plot_single_box_plot_series(entradas_teste, title='Entrada de teste')
+            # plot_multiple_box_plot_series([series_targets, series_forecasts])
 
-        # Calcular os erros relativos
-        relative_errors = np.abs((forecast - df.values[-1]) / forecast)
+            # Calcular o MMRE
+            mmre = np.mean(relative_errors)
+            results_df.loc[results_df['Data_index'] == data_index, f'VAR_result ({steps_ahead} steps ahead)'] = mmre
 
-        # Calcular o MMRE
-        mmre = np.mean(relative_errors)
-        results_df.loc[results_df['Data_index'] == data_index, f'VAR_result ({steps_ahead} steps ahead)'] = mmre
-
-        print("Mean Magnitude of Relative Errors (MMRE):", mmre)
-        print('#' * 80)
+            print("Mean Magnitude of Relative Errors (MMRE):", mmre)
+            print('#' * 80)
 results_df.to_csv(caminho_de_saida, index=False)
