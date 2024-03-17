dff298be49d4f81d512fb6737225e739bd48b4aa
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2024-03-17 09:25:15 -0300

    Usando o drive também nas redes neurais, função separada para usar o drive e usando subpastas

--- utils.py
+++ utils.py
@@ -23,6 +23,68 @@ utils = importr('utils')
 np_config.enable_numpy_behavior()
 
 
+
+def cria_ou_atualiza_arquivo_no_drive(drive, id_pasta, caminho_arquivo_drive, caminho_local_arquivo):
+    caminho_arquivo_drive = caminho_arquivo_drive.replace('\\', '/')
+    if '/' in caminho_arquivo_drive:
+        parent_id = cria_caminho_no_drive(drive, id_pasta, '/'.join(caminho_arquivo_drive.split('/')[:-1]))
+        nome_arquivo = caminho_arquivo_drive.split('/')[-1]
+    else:
+        parent_id = id_pasta
+        nome_arquivo = caminho_arquivo_drive
+    file_list = drive.ListFile({'q': f"'{parent_id}' in parents and trashed=false"}).GetList()
+    file_exists = False
+    existing_file_id = None
+    for file in file_list:
+        if file['title'] == nome_arquivo:
+            file_exists = True
+            existing_file_id = file['id']
+            break
+
+    if file_exists:
+        # Se o arquivo existe, atualiza o conteúdo
+        file = drive.CreateFile({'id': existing_file_id})
+    else:
+        # Se o arquivo não existe, cria um novo arquivo
+        file = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": parent_id}]})
+
+    # Define o conteúdo do arquivo
+    file.SetContentFile(caminho_local_arquivo)
+
+    # Faz o upload ou atualização para o Drive
+    file.Upload()
+
+
+def cria_caminho_no_drive(drive, id_pasta_raiz, caminho):
+    caminho = caminho.replace('\\', '/')
+    if caminho.endswith('/'):
+        caminho = caminho[:-1]
+    if '/' in caminho:
+        nome_pasta_atual = caminho.split('/')[0]
+        proximas_pastas = caminho.split('/')[1:]
+    else:
+        nome_pasta_atual = caminho
+        proximas_pastas = []
+    file_list = drive.ListFile({'q': f"'{id_pasta_raiz}' in parents and trashed=false"}).GetList()
+    file_exists = False
+    current_folder_id = None
+    for file in file_list:
+        if file['title'] == nome_pasta_atual:
+            file_exists = True
+            current_folder_id = file['id']
+            break
+    if not file_exists:
+        # Se o arquivo não existe, cria um novo arquivo
+        new_folder = drive.CreateFile({"title": nome_pasta_atual, "parents": [{"id": id_pasta_raiz}], 'mimeType': 'application/vnd.google-apps.folder'})
+        new_folder.Upload()
+        current_folder_id = new_folder['id']
+    if proximas_pastas:
+        id_pasta_interna = cria_caminho_no_drive(drive, current_folder_id, '/'.join(proximas_pastas))
+        return id_pasta_interna
+    else:
+        return current_folder_id
+
+
 class MMRE_Loss(Loss):
     def __init__(self, inverse_normalizations):
         self.inverse_normalizations = inverse_normalizations
