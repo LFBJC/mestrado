1d5d76ef330c5cb805c32f989327c9112caf4c8b
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2024-06-13 17:52:09 -0300

    Usando 70, 15, 15 em vez de 50, 25, 25 por serem poucos dados

--- tratamento_dos_dados_brutos/demanda_energetica.py
+++ tratamento_dos_dados_brutos/demanda_energetica.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 
 pasta_dos_dados = r"E:/mestrado/Pesquisa/Dados reais/Dados brutos/demanda energética - kaggle"
-pasta_de_saida = r"E:/mestrado/Pesquisa/Dados reais/Dados tratados"
+pasta_de_saida = r"E:/mestrado/Pesquisa/Dados reais/Dados tratados/demanda energética - kaggle"
 df = pd.DataFrame()
 for csv_name in os.listdir(pasta_dos_dados):
     df = pd.concat([df, pd.read_csv(f"{pasta_dos_dados}/{csv_name}")])
 
 df["SETTLEMENTDATE"] = df["SETTLEMENTDATE"].str.replace(" ([0-9]+:)+[0-9]+", "", regex=True)
+# print(df.groupby("SETTLEMENTDATE")['TOTALDEMAND'].min())
 grouped = pd.DataFrame({
     'whislo': df.groupby("SETTLEMENTDATE")["TOTALDEMAND"].min(),
     'q1': df.groupby("SETTLEMENTDATE")["TOTALDEMAND"].apply(lambda data: np.quantile(data, 0.25)),
@@ -16,7 +17,7 @@ grouped = pd.DataFrame({
     'q3': df.groupby("SETTLEMENTDATE")["TOTALDEMAND"].apply(lambda data: np.quantile(data, 0.75)),
     'whishi': df.groupby("SETTLEMENTDATE")["TOTALDEMAND"].max()
 })
-s0, s1 = int(0.5*grouped.shape[0]), int(0.75*grouped.shape[0])
+s0, s1 = int(0.7*grouped.shape[0]), int(0.85*grouped.shape[0])
 train_data, val_data, test_data = grouped.iloc[:s0], grouped.iloc[s0:s1], grouped.iloc[s1:]
 train_data.to_csv(f"{pasta_de_saida}/train.csv")
 val_data.to_csv(f"{pasta_de_saida}/val.csv")
