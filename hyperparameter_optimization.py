e512ca2332bcd633514816296aca10b69f3be39c
Luiz Felipe de Barros Jordão Costa <lfbjc@cin.ufpe.br>
2024-09-13 19:33:47 -0300

    Como vou usar uma máquina melhor, talvez não precise mexer no espaço de busca para o último dataset, somente para o penúltimo
    This reverts commit f03a6b5532be2e8688c2bd46e8c825252fe6d606.
    
    Revert "O último dataset precisa ocupar menos memória"
    
    This reverts commit 65d2455cc9e81899441b37024188cefbd45db33f.

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -33,8 +33,8 @@ def objective_cnn(trial, study, train_data, val_data, pasta_base_saida, caminho_
     else:
         out_size = len(train_data[0])
     print(f'OUT SIZE: {out_size}')
-    win_size = trial.suggest_int('win_size', 10, min(len(train_data)//10, 3000))
-    filters_conv_1 = trial.suggest_int('filters_conv_1', 2, 5)
+    win_size = trial.suggest_int('win_size', 10, min(len(train_data)//10, 13000))
+    filters_conv_1 = trial.suggest_int('filters_conv_1', 2, 10)
     kernel_size_conv_1 = trial.suggest_categorical('kernel_size_conv_1', available_kernel_sizes)
     activation_conv_1 = trial.suggest_categorical('activation_conv_1', ['relu', 'elu', 'sigmoid', 'linear', 'tanh', 'swish'])
     pool_size_1 = trial.suggest_categorical('pool_size_1', [(2, 1), (3, 1)])
@@ -274,10 +274,10 @@ cols_alvo = {
     # "demanda energética - kaggle": "TOTALDEMAND",
     # "cafe": "money",
     # "beijing": "pm2.5",
-    # "KAGGLE - HOUSE HOLD ENERGY CONSUMPTION": "USAGE",
+    "KAGGLE - HOUSE HOLD ENERGY CONSUMPTION": "USAGE",
     "WIND POWER GERMANY": "MW",
 }
-steps_ahead_list = [1]
+steps_ahead_list = [1, 5, 20]
 n_trials = 100
 objective_by_model_type = {
     'LSTM': objective_lstm,
